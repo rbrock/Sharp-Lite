8f0d9ea22bf6bbe047281bf6ed54539ef9b5fb87
rbrock@X-WING <rodolphobrock@gmail.com>
2014-04-22 17:53:23 -0300

    Added NuGet Packages projects, build and publish to NuGet site.

--- SharpLiteSrc/app/SharpLite.EntityFrameworkProvider.NuGet/Properties/AssemblyInfo.cs
+++ SharpLiteSrc/app/SharpLite.EntityFrameworkProvider.NuGet/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpLite.NHibernateProvider")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpLite.EntityFrameworkProvider.NuGet")]
+[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("rbrock")]
-[assembly: AssemblyProduct("SharpLite.NHibernateProvider")]
-[assembly: AssemblyCopyright("Copyright © rbrock 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharpLite.EntityFrameworkProvider.NuGet")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d9d389c3-d047-4039-82bd-558afa8e9a97")]
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
